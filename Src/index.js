d2981dd3a9b37b16c5b5ca56474039f4f4c55b32
Difalt1 <Difalt3@gmail.com>
2021-10-12 12:22:12 +0000

    jestem piékny

---
+++ Src/index.js
@@ -0,0 +1,37 @@
+import { render } from "ejs";
+import React from "react";
+import ReactDOM from "react-dom"
+
+class Game extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+
+        )
+    }
+}
+class Tile extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+
+        )
+    }
+}
+class Grid extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+
+        )
+    }
+}
+
+
+ReactDOM.render(app, document.getElementById("root"));
